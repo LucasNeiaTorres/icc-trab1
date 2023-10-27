3d7c4f837bb9c2eacc9fbb122d6212d47b132208
LeonardooBecker <leonardobecker79@gmail.com>
2023-10-27 13:56:41 -0300

    atulizacao script :devil:

--- ep04/testes.sh
+++ ep04/testes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash                                                                                                                                                                                                                                   
 
-METRICA="FLOPS_DP MEM CACHE ENERGY FLOPS_AVX"
+METRICA="FLOPS_DP L3 CACHE ENERGY FLOPS_AVX"
 CPU=3
 
 echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
@@ -8,7 +8,7 @@ echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
 make
 for k in $METRICA
 do
-    likwid-perfctr -C ${CPU} -g ${k} -m ./matmult > ${k}.log # adicionar tamanhos de matriz de teste
+    likwid-perfctr -C ${CPU} -g ${k} -m ./matmult 8 > ${k}.log # adicionar tamanhos de matriz de teste
 done
 
 echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor 
