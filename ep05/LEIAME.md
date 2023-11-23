2bc34f7ceec18ad974fa9926da110811d95e14c0
Lucas Néia <lucasneia07@gmail.com>
2023-11-22 23:28:43 -0300

    pronto :zap:

---
+++ ep05/LEIAME.md
@@ -0,0 +1,21 @@
+Leonardo Becker de Oliveira - GRR20211779
+Lucas Néia Torres - GRR20210570
+
+# Método de Integração de Monte Carlo e o Método dos Retângulos
+
+## Como rodar
+Para rodar o programa, digite ./monteCarlo n a b
+
+## Quantos pontos deveriam ser usados no método dos Retângulos para que seu cálculo fosse o mais próximo possível do resultado gerado por Monte Carlos para 10^7 amostras? 
+Para a comparação, foi feito os seguintes testes para n = 2, a = 0 e b = 1.
+10^3 pontos: 2.628336
+10^4 pontos: 2.632833
+10^5 pontos: 2.633283
+10^6 pontos: 2.633328
+10^7 pontos: 2.633333
+10^8 pontos: 2.633333
+
+Tendo em vista que o Monte Carlo dá 2.632441, com 10^4 pontos é onde o método dos Retângulos mais se aproxima do método de Monte Carlo com 10^7 amostras.
+
+## Resultados
+A saída do programa demonstra os resultados e os tempos de execução do método dos retângulos para n e 2, já o de Monte Carlo está para n, 2, 4 e 8 para fins de comparação.
--- ep05/monteCarlo.c
+++ ep05/monteCarlo.c
@@ -5,84 +5,82 @@
 
 #include "utils.h"
 
-#define NRAND    ((double)rand() / RAND_MAX)
-#define SRAND(a) srand(a)
-
-// Função Styblinski-Tang de n variáveis
 double styblinskiTang(double x[], int n)
 {
     double sum = 0.0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         sum += (pow(x[i], 4) - 16 * pow(x[i], 2) + 5 * x[i]) / 2.0;
     }
     return sum;
 }
 
+double random_double(double a, double b)
+{
+    return a + ((double)random() / RAND_MAX) * (b - a);
+}
 
-// Função para calcular a integral pelo Método dos Retângulos (n dimensões)
 double retangulos_nD(double a, double b, int npontos, int n)
 {
     double h = (b - a) / npontos;
-    double resultado;
-    double soma = 0;
+    double resultado = 0.0;
+    double soma = 0.0;
 
     printf("Método dos Retângulos (%d dimensões).\n", n);
-    printf("a = (%f), b = (%f), n = (%d), h = (%lg)\n", a, b, npontos, h);
+
+    srandom(20232);
 
     double t_inicial = timestamp();
 
-    // Cálculo da integral pelo Método dos Retângulos
     for (int i = 0; i < npontos; i++)
     {
         double x[n];
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++)
+        {
             x[j] = a + i * h;
         }
         soma += h * styblinskiTang(x, n);
     }
 
-    resultado = soma;
+    resultado = fabs(soma);
 
     double t_final = timestamp();
-    printf("Tempo decorrido: %f seg.\n", t_final - t_inicial);
+    printf("Tempo decorrido: %f seg.\n", (t_final - t_inicial) / 1000);
 
     return resultado;
 }
 
-// Função para calcular a integral pelo Método de Monte Carlo (n dimensões)
 double monteCarlo_nD(double a, double b, int namostras, int n)
 {
-    double resultado;
+    double resultado = 0.0;
     double soma = 0.0;
 
     printf("Método de Monte Carlo (%d dimensões).\n", n);
-    printf("a = (%f), b = (%f), n = (%d), variáveis = %d\n", a, b, namostras, n);
+
+    srandom(20232);
 
     double t_inicial = timestamp();
 
-    // Cálculo da integral pelo Método de Monte Carlo
     for (int i = 0; i < namostras; i++)
     {
         double x[n];
-        for (int j = 0; j < n; j++) {
-            x[j] = a + NRAND * (b - a);
-        }
+        for (int j = 0; j < n; j++)
+            x[j] = random_double(a, b);
         soma += styblinskiTang(x, n);
     }
 
-    resultado = (soma / namostras) * (b - a);
+    resultado = fabs((soma / namostras) * (b - a));
 
     double t_final = timestamp();
-    printf("Tempo decorrido: %f seg.\n", t_final - t_inicial);
+    printf("Tempo decorrido: %f seg.\n", (t_final - t_inicial) / 1000);
 
     return resultado;
 }
 
-
-
 int main(int argc, char **argv)
 {
-    if (argc < 4) {
+    if (argc < 4)
+    {
         printf("Utilização: %s n a b\n", argv[0]);
         return 1;
     }
@@ -90,17 +88,38 @@ int main(int argc, char **argv)
     int n = atoi(argv[1]);
     double a = atof(argv[2]);
     double b = atof(argv[3]);
-    int npontos = 10000000;  // 10^7 pontos para o Método dos Retângulos
-    int namostras = 10000000;  // 10^7 amostras para o Método de Monte Carlo
+    int npontos = 10000000;   // 10^7 pontos para o Método dos Retângulos
+    int namostras = 10000000; // 10^7 amostras para o Método de Monte Carlo
 
-    SRAND(time(NULL));
-
-    // Chamada da função dos Retângulos
     double resultRetangulos = retangulos_nD(a, b, npontos, n);
     printf("Resultado do Método dos Retângulos: %f\n", resultRetangulos);
 
-    // Chamada da função de Monte Carlo
+    printf("----------------------------------------------------------\n");
+
     double resultMonteCarlo = monteCarlo_nD(a, b, namostras, n);
+    printf("Resultado do Método de Monte Carlo: %f\n", resultMonteCarlo);
+
+    printf("----------------------------------------------------------\n");
+
+    if (n != 2)
+    {
+        resultRetangulos = retangulos_nD(a, b, npontos, 2);
+        printf("Resultado do Método dos Retângulos: %f\n", resultRetangulos);
+
+        printf("----------------------------------------------------------\n");
+
+        resultMonteCarlo = monteCarlo_nD(a, b, namostras, 2);
+        printf("Resultado do Método de Monte Carlo: %f\n", resultMonteCarlo);
+
+        printf("----------------------------------------------------------\n");
+    }
+
+    resultMonteCarlo = monteCarlo_nD(a, b, namostras, 4);
+    printf("Resultado do Método de Monte Carlo: %f\n", resultMonteCarlo);
+
+    printf("----------------------------------------------------------\n");
+
+    resultMonteCarlo = monteCarlo_nD(a, b, namostras, 8);
     printf("Resultado do Método de Monte Carlo: %f\n", resultMonteCarlo);
 
     return 0;
